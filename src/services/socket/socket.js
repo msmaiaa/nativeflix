1a6fa47a6496dfe49b82f781adb2970cb19fdfad
msmaiaa <roxx77344@gmail.com>
2020-11-24 10:12:26 -0300

    created config file

--- src/services/socket/socket.js
+++ src/services/socket/socket.js
@@ -1,4 +1,5 @@
-const url = 'http://192.168.0.10:1337'
+import config from '../../../config.json';
+const url = `http://${config.ip}:${config.port}`;
 const io = require("socket.io-client/dist/socket.io")
 const socket = io(url , {transports: ['websocket'], jsonp: false ,query: { type: 'app'}});
 
