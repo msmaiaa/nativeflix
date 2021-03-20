02b66d0153f1ee978045bfa04ab7dc7c6867fee1
msmaiaa <roxx77344@gmail.com>
2021-03-19 23:06:07 -0300

    add eslint and husky

--- App.js
+++ App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Home from './src/components/Home/Home';
-import MediaList from './src/components/MediaList/MediaList';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import MediaDetails from './src/components/MediaDetails/MediaDetails';
+import Home from './src/pages/Home/Home';
+import MediaList from './src/pages/MediaList/MediaList';
+import MediaDetails from './src/pages/MediaDetails/MediaDetails';
 import socket from './src/services/socket/socket';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView style={{flex:1}}>
+    <SafeAreaView style={{ flex: 1 }}>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={Home} options={{headerShown:false}}></Stack.Screen>
-          <Stack.Screen name="MediaList" component={MediaList}></Stack.Screen>
-          <Stack.Screen name="MediaDetails" component={MediaDetails}></Stack.Screen>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="MediaList" component={MediaList} />
+          <Stack.Screen name="MediaDetails" component={MediaDetails} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaView>
   );
 }
-
