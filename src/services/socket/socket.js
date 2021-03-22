9a815c0b30d819656ee51909b09284411a69fef8
msmaiaa <roxx77344@gmail.com>
2021-03-22 00:08:03 -0300

    update axios

--- src/services/socket/socket.js
+++ src/services/socket/socket.js
@@ -1,6 +1,13 @@
+// eslint-disable-next-line import/extensions
 import config from '../../../config.json';
-const url = `http://${config.ip}:${config.port}`;
-const io = require("socket.io-client/dist/socket.io")
-const socket = io(url , {transports: ['websocket'], jsonp: false ,query: { type: 'app'}});
 
-export default socket
+const url = `http://${config.ip}:${config.port}`;
+const io = require('socket.io-client/dist/socket.io');
+
+const socket = io(url, {
+	transports: ['websocket'],
+	jsonp: false,
+	query: { type: 'app' },
+});
+
+export default socket;
