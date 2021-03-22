6a688c9fae690d8b0796765bcae3638cb3807eec
msmaiaa <roxx77344@gmail.com>
2021-03-21 22:40:55 -0300

    create label component

---
+++ src/components/LabelSM/LabelSM.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+type Props = {
+	bgColor: string;
+	title: string | number;
+	width: number | string;
+};
+
+export default function LabelSM({ bgColor, title, width }: Props) {
+	return (
+		<View
+			style={{
+				width,
+				height: 13,
+				marginBottom: 12,
+				elevation: 1,
+				shadowRadius: 2,
+				shadowOffset: { width: 0, height: 0 },
+				shadowOpacity: 2,
+				shadowColor: bgColor,
+				borderWidth: 1,
+				borderRadius: 5,
+				borderColor: '#000000',
+				backgroundColor: '#000000',
+			}}
+		>
+			<Text style={styles.text}>{title}</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	text: {
+		color: '#ffffff',
+		textAlign: 'center',
+		fontSize: 10,
+	},
+});
